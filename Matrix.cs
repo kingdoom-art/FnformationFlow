53d6c89583a9a64a825f707ecc68879746751c54
kingdoom-art <bartov-2016@mail.ru>
2021-10-27 00:04:20 +0500

    прибрал код, сделал возведение в стапень

---
+++ Matrix.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace lab2
+{
+    [Serializable]
+    public class Matrix
+    {
+        public int[][] root_mas;
+        //сумма элементов в столбце
+        public int[] column_mu;
+        //сумма элементов в строке
+        public int[] row_mu;
+        [NonSerialized]
+        public static string mu = "  \u03B4";
+        //степень матрицы
+        public int step = 1;
+        public int count;
+
+
+        public Matrix(List<KeyValuePair<int,int>> mas)
+        {
+            //стадия инициализации
+            count = mas.Select(e => e.Key).Union(mas.Select(e => e.Value)).Count();
+            root_mas = new int[count][];
+            column_mu = new int[count];
+            row_mu = new int[count];
+
+            for(int i = 0; i < count; i++)
+            {
+                root_mas[i] = new int[count];
+            }
+            //заполняем матрицу
+            foreach(var i in mas)
+            {
+                root_mas[i.Key - 1][i.Value - 1] = 1;
+            }
+            //посчитаем всякую всячину
+            WriteMU();
+        }
+
+        public Matrix(int[][] mas, int _step, int _count)
+        {
+            root_mas = mas;
+            step = _step;
+            count = _count;
+            row_mu = new int[count];
+            column_mu = new int[count];
+            WriteMU();
+        }
+
+        public Matrix(int[][] root_mas, int[] column_mu)
+        {
+            this.root_mas = root_mas;
+        }
+
+        private void WriteMU()
+        {
+            //считаем невыговариваемую букву
+            for(int i = 0; i < count; i++)
+            {
+                row_mu[i] = root_mas[i].Sum();
+            }
+
+            for(int i = 0; i < count; i++)
+            {
+                for(int j = 0; j < count; j++)
+                {
+                    column_mu[i]+=root_mas[j][i];
+                }
+            }
+        }
+
+        public void ShowMatrix()
+        {
+            Console.WriteLine("A" + step);
+            Console.Write("   ");
+            for(int i = 0; i < count; i++)
+            {
+                Console.Write(String.Format("{0,3}", i + 1));
+            }
+                
+            Console.WriteLine(mu);
+
+            for(int i = 0; i < count; i++)
+            {
+                Console.Write(String.Format("{0,3}", i + 1));
+                root_mas[i].ToList().ForEach(e => Console.Write(String.Format("{0,3}", e)));
+                Console.WriteLine(String.Format("{0,3}", row_mu[i]));
+            }
+
+            Console.Write(mu);
+            column_mu.ToList().ForEach(e => Console.Write(String.Format("{0,3}", e)));
+            Console.WriteLine();
+        }
+
+        public Matrix Pow(Matrix m1)
+        {
+            int not_zero = 0;
+            int[][] result = new int[count][];
+
+            for(int i = 0; i < count; i++)
+            {
+                result[i] = new int[count];
+                for(int j = 0; j < count; j++)
+                {
+                    for(int k = 0; k < count; k++)
+                    {
+                        result[i][j] += root_mas[i][k] * m1.root_mas[k][j];
+                    }
+                }
+
+                //нужно не пропустить нулевую матрицу
+                not_zero += result[i].Sum();
+            }
+
+            return not_zero != 0 ? new Matrix(result, ++step, count) : null;
+        }
+
+        public Matrix Clone()
+        {
+            Matrix obj = null;
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (FileStream fs = new FileStream("matrix.dat", FileMode.Create))
+            {
+                formatter.Serialize(fs, this);
+            }
+            using (FileStream fs = new FileStream("matrix.dat", FileMode.OpenOrCreate))
+            {
+                obj = (Matrix)formatter.Deserialize(fs);
+            }
+
+            return obj;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,19 +1,94 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace lab2
 {
     class Program
     {
-        static int[][] GetMas()
+        //получение графа формата (Первая_вершина Вторя_вершина)
+        static List<KeyValuePair<int,int>> GetGraph()
         {
-            int[][] result_mas = null;
+            List<KeyValuePair<int, int>> mas = new List<KeyValuePair<int, int>>();
+            try
+            {
+                using (StreamReader str = new StreamReader("Graph.txt"))
+                {
+                    string tmp;
+                    while ((tmp = str.ReadLine()) != null)
+                    {
+                        var tmpMas = tmp.Split(' ').Select(e=>Convert.ToInt32(e)).ToArray();
+                        mas.Add(new KeyValuePair<int, int>(tmpMas[0], tmpMas[1]));
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
-            return result_mas;
+            return mas;
         }
+
+        //Вывод матицы смежности
+        static void ShowMatrix(int[,] _mas)
+        {
+            int row = 0;
+            int column = 0;
+            for(int i = 0; i < _mas.GetLength(0); i++)
+            {
+                if (i == 0)
+                {
+                    Console.Write("   ");
+                    for(int k = 1; k <= _mas.GetLength(0); k++)
+                    {
+                        Console.Write(String.Format("{0,3}", k));
+                    }
+                    Console.WriteLine("");
+                }
+
+                Console.Write(String.Format("{0,3}", i+1));
+
+                for (int j = 0; j < _mas.GetLength(0); j++)
+                {
+                    if (_mas[i, j] != 0) row++;
+                    Console.Write(String.Format("{0,3}",_mas[i,j]));
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static int[][] MultMatrix(ref int[][] matrix)
+        {
+            int[][] matrixResult = new int[matrix.Count()][];
+            for (int i = 0; i < matrix.Count(); ++i)
+            {
+                matrixResult[i] = new int[matrix.Count()];
+                for (int j = 0; j < matrix.Count(); ++j)
+                    for (int k = 0; k < matrix.Count(); ++k)
+                        matrixResult[i][j] += matrix[i][k] * matrix[k][j];
+            }
+            return matrixResult;
+        }
+
         static void Main(string[] args)
         {
 
-            
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            List<Matrix> all_matrix = new List<Matrix>();
+            Matrix m = new Matrix(GetGraph());
+            Matrix n = m.Clone();
+            while(n != null)
+            {
+                all_matrix.Add(n.Clone());
+                n = n.Pow(m);
+            }
+
+            foreach(var c in all_matrix)
+            {
+                c.ShowMatrix();
+            }
         }
     }
 }
